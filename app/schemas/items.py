793b69a894cf60a31a6a0f5106215d626880cec7
ramenisneat <rahulmyana42@gmail.com>
2023-09-29 18:51:15 +0000

    ahhh didn't have a gitignore. Big L

--- app/schemas/items.py
+++ app/schemas/items.py
@@ -5,6 +5,9 @@ from datetime import datetime
 #This is an example of an item in the inventory.
 #Using this model the API can read requests from the website and return back
 #information
+#
+#This is an example of Object Oriented Design: we can add validators, make child
+#models, custom funcs, etc.
 class Item(BaseModel):
     name: str
     description: str
@@ -12,4 +15,5 @@ class Item(BaseModel):
     dateAdded: datetime
     dateEdited: datetime
     tags: List[str]
+    #add an id for the db
 
